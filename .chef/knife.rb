b9f4d1000bc9afd7bd5657301f0bc119ef7f4f22
Justin Locsei <justin.locsei@gmail.com>
2016-01-13 20:18:38 -0500

    Update the Chef organization name

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,12 +1,12 @@
 current_dir = File.dirname(__FILE__)
-chef_user_name = ENV["CHITON_CHEF_USER_NAME"]
+chef_user_name = ENV["CYB_CHEF_USER_NAME"]
 
 log_level    :info
 log_location STDOUT
 
-chef_server_url        "https://api.chef.io/organizations/chiton"
+chef_server_url        "https://api.chef.io/organizations/coveryourbasics"
 client_key             "#{current_dir}/#{chef_user_name}.pem"
 cookbook_path          ["#{current_dir}/../chef/cookbooks"]
 node_name              "#{chef_user_name}"
-validation_client_name "chiton-validator"
-validation_key         "#{current_dir}/chiton-validator.pem"
+validation_client_name "coveryourbasics-validator"
+validation_key         "#{current_dir}/coveryourbasics-validator.pem"
