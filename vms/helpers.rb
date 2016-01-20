b4ab0f9c81c7479bc53a1a27ac0aaeff4c7cf98f
Justin Locsei <justin.locsei@gmail.com>
2016-01-19 20:08:35 -0500

    Add a centralized mapping of addresses for VMs

--- vms/helpers.rb
+++ vms/helpers.rb
@@ -1,5 +1,12 @@
 module Helpers
 
+  ADDRESSES = {
+    :himation => {
+      :ip => "10.1.0.0",
+      :port => 8080
+    }
+  }
+
   # Return the absolute path to an Ansible file
   #
   # @param [String] path The path to the file, relative to the Ansible root
