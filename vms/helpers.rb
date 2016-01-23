6aca48e679d7e8de2aea4b88b433ed2c4d5fed13
Justin Locsei <justin.locsei@gmail.com>
2016-01-22 20:10:32 -0500

    Use the custom private key to connect to the Himation VM

--- vms/helpers.rb
+++ vms/helpers.rb
@@ -23,4 +23,11 @@ module Helpers
     File.expand_path("#{ENV['CYB_PROJECTS_DIR']}/#{name}")
   end
 
+  # Return the path to the private key file for the Vagrant user
+  #
+  # @return [String]
+  def self.private_key_path
+    ENV["CYB_VAGRANT_KEY_PRIVATE"]
+  end
+
 end
