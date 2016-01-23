f53376fc64c5497202b6bc183d00d842cb3c57c6
Justin Locsei <justin.locsei@gmail.com>
2016-01-22 19:49:11 -0500

    Use a Packer variable to manage the path to the VBox guest additions

--- packer/script/virtualbox.sh
+++ packer/script/virtualbox.sh
@@ -4,9 +4,8 @@ USER=$CYB_SSH_USERNAME
 HOME_DIR=/home/$USER
 
 # Install the VirtualBox guest additions
-VBOX_VERSION=$(cat "$HOME_DIR/.vbox_version")
-mount -o loop "$HOME_DIR/VBoxGuestAdditions_$VBOX_VERSION.iso" /mnt
+mount -o loop "$HOME_DIR/$CYB_GUEST_ADDITIONS_PATH" /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
-rm -rf "$HOME_DIR/VBoxGuestAdditions_$VBOX_VERSION.iso"
+rm -rf "${HOME_DIR:?}/$CYB_GUEST_ADDITIONS_PATH"
 rm -f "$HOME_DIR/.vbox_version"
