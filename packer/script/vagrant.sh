b0774be8b975b62936e285c79f0ac6171c8e9f8d
Justin Locsei <justin.locsei@gmail.com>
2016-01-25 19:23:53 -0500

    Install cachefilesd in the base VM

--- packer/script/vagrant.sh
+++ packer/script/vagrant.sh
@@ -11,3 +11,8 @@ mkdir -pm 700 "$SSH_DIR"
 cat "$CYB_SSH_KEY_PATH" > "$SSH_DIR/authorized_keys"
 chmod 0600 "$SSH_DIR/authorized_keys"
 chown -R "$USER:$USER" "$SSH_DIR"
+
+# Use cachefilesd to improve synced-folder performance over NFS
+apt-get -y install cachefilesd
+sed -i "s/^#RUN=yes/RUN=yes/" /etc/default/cachefilesd
+update-rc.d cachefilesd enable
