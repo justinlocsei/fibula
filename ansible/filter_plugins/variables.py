dc8bed4313b72f98b4bfd6f2ea0f87269eb1e156
Justin Locsei <justin.locsei@gmail.com>
2016-02-09 20:35:01 -0500

    Add an Ansible filter for ensuring that a variable is not null

---
+++ ansible/filter_plugins/variables.py
@@ -0,0 +1,20 @@
+from ansible import errors
+from jinja2.runtime import Undefined
+
+
+def require_value(value):
+    """Require a non-empty value for a variable"""
+    if isinstance(value, Undefined):
+        raise errors.AnsibleFilterError("Variable must be defined.")
+    if value is None:
+        raise errors.AnsibleFilterError("Variable must not be null.")
+    return value
+
+
+class FilterModule(object):
+    """Custom Jinja2 filters for Ansible"""
+
+    def filters(self):
+        return {
+            "require_value": require_value
+        }
