424e7eea17ca847be8e8076796588a07a5a4218e
Justin Locsei <justin.locsei@gmail.com>
2016-03-25 10:29:46 -0400

    Use a dedicated object for logging infrastructure actions

--- infrastructure/fibula/actions/base.py
+++ infrastructure/fibula/actions/base.py
@@ -3,9 +3,17 @@ import os
 import click
 import digitalocean
 
+from fibula.communicator import Communicator
+
 
 class BaseAction:
     """An abstract base action for interacting with the Digital Ocean API."""
+
+    log_prefix = None
+
+    def __init__(self):
+        """Create a new action."""
+        self.ui = Communicator(label=self.log_prefix)
 
     @property
     def do(self):
@@ -31,37 +39,3 @@ class BaseAction:
         if not hasattr(self, '_token'):
             self._token = os.environ['CYB_DO_API_TOKEN']
         return self._token
-
-    def log(self, message, created=False, deleted=False, updated=False):
-        """Log a message for a change caused by the action.
-
-        Args:
-            message (str): The info message to display
-            created (bool): Whether the change created an object
-            deleted (bool): Whether the change deleted an object
-            updated (bool): Whether the change updated an object
-        """
-        icon = u'\u00b7'
-        formatting = {
-            'bold': created or deleted or updated
-        }
-
-        if created:
-            formatting['fg'] = 'green'
-            icon = u'\u2713'
-        elif deleted:
-            formatting['fg'] = 'red'
-            icon = u'\u2717'
-        elif updated:
-            formatting['fg'] = 'yellow'
-            icon = '-'
-
-        click.echo(click.style('%s %s' % (icon, message), **formatting))
-
-    def warn(self, message):
-        """Show a warning message.
-
-        Args:
-            message (str): The warning message to display
-        """
-        click.echo(click.style(u'\u26a0 %s' % message, fg='yellow', bold=True))
---
+++ infrastructure/fibula/communicator.py
@@ -0,0 +1,91 @@
+import click
+
+
+class Communicator:
+    """A utility for displaying messages to the user and receiving input."""
+
+    def __init__(self, label=None):
+        """Create a new logger.
+
+        Keyword Args:
+            label (str): An optional label for the logger
+        """
+        self.label = label
+
+    def group(self, name):
+        """Create a new communicator with a group label.
+
+        Args:
+            name (str): The label for the subgroup
+
+        Returns:
+            fibula.communicator.Communicator
+        """
+        if self.label:
+            label = '%s/%s' % (self.label, name)
+        else:
+            label = name
+
+        return Communicator(label=label)
+
+    def info(self, message):
+        """Show an informational message."""
+        click.echo(self._format(message, u'\u00b7'))
+
+    def create(self, message):
+        """Show a message for an action that created an object."""
+        output = self._format(message, '+')
+        click.echo(click.style(output, fg='green', bold=True))
+
+    def delete(self, message):
+        """Show a message for an action that deleted an object."""
+        output = self._format(message, '-')
+        click.echo(click.style(output, fg='red', bold=True))
+
+    def update(self, message):
+        """Show a message for an action that updated an object."""
+        output = self._format(message, '=')
+        click.echo(click.style(output, fg='yellow', bold=True))
+
+    def skip(self, message):
+        """Show a message indicating a skipped action."""
+        output = self._format(message, '#')
+        click.echo(click.style(output, fg='yellow'))
+
+    def error(self, message):
+        """Show an error message."""
+        output = self._format(message, '!')
+        click.echo(click.style(output, fg='white', bg='red'))
+
+    def warn(self, message):
+        """Show a warning."""
+        output = self._format(message, '!')
+        click.echo(click.style(output, fg='white', bg='yellow'))
+
+    def confirm(self, prompt):
+        """Request confirmation from the user for an action.
+
+        Args:
+            prompt (str): The confirmation prompt
+
+        Returns:
+            bool: Whether the user accepted
+        """
+        return click.confirm(self._format(prompt))
+
+    def _format(self, message, icon=' '):
+        """Return a message formatted for display.
+
+        Args:
+            message (str): The message to show
+
+        Keyword Args:
+            icon (str): The icon to use with the message
+
+        Returns:
+            str: The formatted message
+        """
+        if self.label:
+            return '%s [%s] %s' % (icon, self.label, message)
+        else:
+            return '%s %s' % (icon, message)
