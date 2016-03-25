950148b38435416bf4735d4b3d08d7420375ec1f
Justin Locsei <justin.locsei@gmail.com>
2016-03-25 00:37:57 -0400

    Create a skeleton for infrastructure scripts

---
+++ infrastructure/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='fibula',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click==6.4'
+    ],
+    entry_points="""
+        [console_scripts]
+        fibula=fibula.cli:cli
+    """
+)
