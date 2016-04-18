75fe04204099848b42eb3396b87dc899e0c0196f
Justin Locsei <justin.locsei@gmail.com>
2016-04-17 20:02:10 -0400

    Add an Ansible filter to create a newline-separated list

--- ansible/filter_plugins/lists.py
+++ ansible/filter_plugins/lists.py
@@ -15,10 +15,23 @@ def flatten(value):
     return chain.from_iterable(value)
 
 
+def newline_list(value):
+    """Build an array of all newline-separated items in a string.
+
+    Args:
+        value (str): A string containing newlines
+
+    Returns:
+        list: The newline-separated items in the string
+    """
+    return value.strip().splitlines()
+
+
 class FilterModule(object):
     """Custom Jinja2 filters for working with lists."""
 
     def filters(self):
         return {
-            'flatten': flatten
+            'flatten': flatten,
+            'newline_list': newline_list
         }
